ef8654c3db66273df60048a2510e4935be5df1f1
tigertang <tigertang.zju@qq.com>
2025-08-25 09:12:50 +0800

    Fix mnist dataset

--- mytorch/utils/data/mnist_dataset.py
+++ mytorch/utils/data/mnist_dataset.py
@@ -5,6 +5,7 @@ import gzip
 import os
 
 import numpy as np
+from tqdm import tqdm
 
 from mytorch.utils.data.dataset import Dataset
 
@@ -34,47 +35,64 @@ class MNISTDataset(Dataset):
     ):
         super().__init__()
         self.root = root
+        self.train = train
         os.makedirs(osp.join(self.root, "MNIST"), exist_ok=True)
         if download:
-            self._download()
-        self._read_bytes(train)
-        self._size = struct.unpack(">i", self.x_buf[4:8])[0]
+            self._download_and_save_npy()
+        self._load_npy()
         self._transform = transform
         self._target_transform = target_transform
 
-    def _read_bytes(self, train):
-        prefix = "train" if train else "test"
-        with open(
-            osp.join(self.root, "MNIST", self.FILENAMES[f"{prefix}_x"][:-3]), "rb"
-        ) as f:
-            self.x_buf = f.read()
-        with open(
-            osp.join(self.root, "MNIST", self.FILENAMES[f"{prefix}_y"][:-3]), "rb"
-        ) as f:
-            self.y_buf = f.read()
+    def _load_npy(self):
+        prefix = "train" if self.train else "test"
+        self.x = np.load(osp.join(self.root, "MNIST", f"{prefix}_x.npy"))
+        self.y = np.load(osp.join(self.root, "MNIST", f"{prefix}_y.npy"))
 
-    def _download(self):
+    def _download_and_save_npy(self):
+        folder = osp.join(self.root, "MNIST")
         for filename in self.FILENAMES.values():
-            _download_single_file(filename, osp.join(self.root, "MNIST"))
+            _download_single_file(filename, folder)
+        for train in [False, True]:
+            prefix = "train" if train else "test"
+            imgs = []
+            labels = []
+            x_buf, y_buf = self._read_bytes(folder=folder, train=train)
+            size = struct.unpack(">i", x_buf[4:8])[0]
+            for i in tqdm(range(size)):
+                imgs.append(self._get_img(x_buf, i).reshape((1, 28, 28)))
+                labels.append(self._get_label(y_buf, i))
+            np.save(osp.join(folder, f"{prefix}_x.npy"), np.concatenate(imgs, axis=0))
+            np.save(osp.join(folder, f"{prefix}_y.npy"), np.array(labels))
 
-    def _get_img(self, idx):
+    @classmethod
+    def _read_bytes(cls, folder, train):
+        prefix = "train" if train else "test"
+        with open(osp.join(folder, cls.FILENAMES[f"{prefix}_x"][:-3]), "rb") as f:
+            x_buf = f.read()
+        with open(osp.join(folder, cls.FILENAMES[f"{prefix}_y"][:-3]), "rb") as f:
+            y_buf = f.read()
+        return x_buf, y_buf
+
+    @staticmethod
+    def _get_img(x_buf, idx):
         offset = idx * 784
-        data = struct.unpack_from(">784B", self.x_buf[16:], offset)
+        data = struct.unpack_from(">784B", x_buf, offset + 16)
         return np.array(data).reshape(28, 28)
 
-    def _get_label(self, idx):
-        label = struct.unpack_from(">B", self.y_buf, idx + 8)
+    @staticmethod
+    def _get_label(y_buf, idx):
+        label = struct.unpack_from(">B", y_buf, idx + 8)
         return label[0]
 
     def __getitem__(self, index):
-        img = self._get_img(index)
+        img = self.x[index]
         if self._transform is not None:
             img = self._transform(img)
 
-        label = self._get_label(index)
+        label = self.y[index]
         if self._target_transform is not None:
             label = self._target_transform(label)
         return img, label
 
     def __len__(self):
-        return self._size
+        return self.x.shape[0]
