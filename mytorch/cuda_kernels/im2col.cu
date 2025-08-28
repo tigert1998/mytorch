6683912db144e7edc1fdf6bae2b85477d08f5910
tigertang <tigertang.zju@qq.com>
2025-08-29 00:55:04 +0800

    Optimize reverse im2col input

--- mytorch/cuda_kernels/im2col.cu
+++ mytorch/cuda_kernels/im2col.cu
@@ -27,7 +27,7 @@ __global__ void im2col_input_reference(int batch_size, int input_h, int input_w,
         x[i * x_cols_padded + j] = 0;
       } else if (j == x_cols - 1 && bias != nullptr) {
         int channels_idx = j / (kernel_size_h * kernel_size_w);
-        x[i * x_cols_padded + j] = bias[channels_idx];
+        x[i * x_cols_padded + j] = 1;
       } else {
         int output_h_idx = i / output_w % output_h;
         int output_w_idx = i % output_w;
@@ -131,35 +131,38 @@ __global__ void reverse_im2col_input_reference(
     int padding_h, int padding_w, T *input, T *bias, T *x) {
   int output_h = (input_h + padding_h * 2 - kernel_size_h) / stride_h + 1;
   int output_w = (input_w + padding_w * 2 - kernel_size_w) / stride_w + 1;
+  int x_rows = batch_size * output_h * output_w;
+  int x_cols =
+      in_channels * kernel_size_h * kernel_size_w + int(bias != nullptr);
+  int x_cols_padded = (x_cols + 3) / 4 * 4;
 
   int xid = blockIdx.x * blockDim.x + threadIdx.x;
   int yid = blockIdx.y * blockDim.y + threadIdx.y;
-  int zid = blockIdx.z * blockDim.z + threadIdx.z;
 
-  if (xid >= batch_size || yid >= output_h || zid >= output_w) return;
+  if (xid >= x_rows || yid >= x_cols_padded) return;
 
-  int x_rows = batch_size * output_h * output_w;
-  int x_cols =
-      kernel_size_h * kernel_size_w * in_channels + int(bias != nullptr);
-  int x_cols_padded = (x_cols + 3) / 4 * 4;
+  int x_row_offset = x_cols_padded * xid;
 
-  int x_row_offset = x_cols_padded * ((xid * output_h + yid) * output_w + zid);
+  for (int i = xid; i < x_rows; i += blockDim.x * gridDim.x)
+    for (int j = yid; j < x_cols - int(bias != nullptr);
+         j += blockDim.y * gridDim.y) {
+      int output_h_idx = i / output_w % output_h;
+      int output_w_idx = i % output_w;
+      int batch_idx = i / (output_h * output_w);
+      int channels_idx = j / (kernel_size_h * kernel_size_w);
+      int kernel_size_h_idx = j / kernel_size_w % kernel_size_h;
+      int kernel_size_w_idx = j % kernel_size_w;
+      int input_x = output_h_idx * stride_h + kernel_size_h_idx - padding_h;
+      int input_y = output_w_idx * stride_w + kernel_size_w_idx - padding_w;
+      bool is_padding = input_x < 0 || input_y < 0 || input_x >= input_h ||
+                        input_y >= input_w;
 
-  for (int k = 0; k < in_channels; k++)
-    for (int i = 0; i < kernel_size_h; i++)
-      for (int j = 0; j < kernel_size_w; j++) {
-        int input_x = yid * stride_h + i - padding_h;
-        int input_y = zid * stride_w + j - padding_w;
-        bool is_padding = input_x < 0 || input_y < 0 || input_x >= input_h ||
-                          input_y >= input_w;
-        if (!is_padding) {
-          int x_col_idx = (k * kernel_size_h + i) * kernel_size_w + j;
-          atomicAdd(
-              &input[((xid * in_channels + k) * input_h + input_x) * input_w +
-                     input_y],
-              x[x_row_offset + x_col_idx]);
-        }
+      if (!is_padding) {
+        input[((batch_idx * in_channels + channels_idx) * input_h + input_x) *
+                  input_w +
+              input_y] += x[i * x_cols_padded + j];
       }
+    }
 }
 
 extern "C" __global__ void reverse_im2col_input_reference_fp32(
