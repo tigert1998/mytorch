15afa098c61d1f92448723b5155f79ddd34f1948
tigertang <tigertang.zju@qq.com>
2025-08-30 15:50:12 +0800

    Add optimizer.state_dict

--- mytorch/optim/optimizer.py
+++ mytorch/optim/optimizer.py
@@ -34,3 +34,41 @@ class Optimizer:
         for g in self.param_groups:
             for param in g["params"]:
                 param.grad = None
+
+    def state_dict(self):
+        state = {}
+
+        id_map = {}
+
+        def convert_group(g):
+            new_g = {k: g[k] for k in g.keys() if k != "params"}
+            new_params = []
+            for p in g["params"]:
+                i = id_map.get(p)
+                if i is None:
+                    i = id_map[p] = id(p)
+                new_params.append(i)
+            new_g["params"] = new_params
+            return new_g
+
+        for k in self.state:
+            state[id(k)] = self.state[k]
+
+        param_groups = [convert_group(g) for g in self.param_groups]
+        return {"state": state, "param_groups": param_groups}
+
+    def load_state_dict(self, state_dict):
+        state = state_dict["state"]
+        param_groups = state_dict["param_groups"]
+
+        assert len(param_groups) == len(self.param_groups)
+        for i in range(len(param_groups)):
+            assert len(param_groups[i]["params"]) == len(self.param_groups[i]["params"])
+
+        for i in range(len(self.param_groups)):
+            for j in range(len(self.param_groups[i]["params"])):
+                idx = param_groups[i]["params"][j]
+                if state.get(idx) is None:
+                    continue
+                tensor = self.param_groups[i]["params"][j]
+                self.state[tensor] = state[idx]
