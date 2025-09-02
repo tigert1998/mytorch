c8eac636de59aead3ec4f4ca331a3fbaa58c0727
tigertang <tigertang.zju@qq.com>
2025-09-02 17:16:42 +0800

    Update tokenizer api

--- mytorch/text/tokenizer.py
+++ mytorch/text/tokenizer.py
@@ -1,9 +1,63 @@
 import heapq
-from typing import Dict, Set, List, Optional, Tuple, Generator
+from typing import Dict, List, Optional, Tuple
 from collections import defaultdict
 import regex as re
+from functools import lru_cache
+import json
 
 from tqdm import tqdm
+
+
+@lru_cache
+def gpt2_bytes_to_unicode() -> dict[int, str]:
+    """
+    Returns a mapping between every possible byte (an integer from 0 to 255) to a
+    printable unicode string character representation. This function is taken
+    from the GPT-2 code.
+
+    For example, `chr(0)` is `\x00`, which is an unprintable character:
+
+    >>> chr(0)
+    '\x00'
+    >>> print(chr(0))
+
+    As a result, this function returns a dictionary `d` where `d[0]` returns `Ā`.
+    The bytes that are visually printable keep their original string representation [1].
+    For example, `chr(33)` returns `!`, and so accordingly `d[33]` returns `!`.
+    Note in particular that the space character `chr(32)` becomes `d[32]`, which
+    returns 'Ġ'.
+
+    For unprintable characters, the function shifts takes the integer representing
+    the Unicode code point of that character (returned by the Python `ord`) function
+    and shifts it by 256. For example, `ord(" ")` returns `32`, so the the space character
+    ' ' is shifted to `256 + 32`. Since `chr(256 + 32)` returns `Ġ`, we use that as the
+    string representation of the space.
+
+    This function can simplify the BPE implementation and makes it slightly easier to
+    manually inspect the generated merges after they're serialized to a file.
+    """
+    # These 188 integers can used as-is, since they are not whitespace or control characters.
+    # See https://www.ssec.wisc.edu/~tomw/java/unicode.html.
+    bs = (
+        list(range(ord("!"), ord("~") + 1))
+        + list(range(ord("¡"), ord("¬") + 1))
+        + list(range(ord("®"), ord("ÿ") + 1))
+    )
+    cs = bs[:]
+    # now get the representations of the other 68 integers that do need shifting
+    # each will get mapped chr(256 + n), where n will grow from 0...67 in the loop
+    # Get printable representations of the remaining integers 68 integers.
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            # If this integer isn't in our list of visually-representable
+            # charcters, then map it to the next nice character (offset by 256)
+            bs.append(b)
+            cs.append(2**8 + n)
+            n += 1
+    characters = [chr(n) for n in cs]
+    d = dict(zip(bs, characters))
+    return d
 
 
 class Merge:
@@ -58,15 +112,26 @@ class BPETokenizer:
     vocab: Dict[int, bytes]
     merges: Dict[int, Merge]
     _vocab_dic: Dict[bytes, int]
+    special_tokens: List[str]
+    _special_token_pattern: str
 
-    def __init__(self, special_tokens=None):
-        self.special_tokens: Optional[List[str]] = (
-            special_tokens if special_tokens is not None else []
-        )
+    def __init__(
+        self,
+        vocab: Optional[Dict[int, bytes]] = None,
+        merges: Optional[Dict[int, Merge]] = None,
+        special_tokens: Optional[List[str]] = None,
+    ):
+        self.special_tokens = special_tokens if special_tokens is not None else []
         if len(self.special_tokens) > 0:
-            self._special_token_pattern = f"({'|'.join(re.escape(s) for s in sorted(self.special_tokens, key=lambda s: -len(s)))})"
+            self._special_token_pattern = self.build_special_token_pattern(
+                self.special_tokens
+            )
         else:
             self._special_token_pattern = None
+
+        self.vocab = vocab if vocab is not None else {}
+        self.merges = merges if merges is not None else {}
+        self._vocab_dic = {v: k for k, v in self.vocab.items()}
 
     def _pre_tokenize(
         self, text_array: List[str], vocab_dic: Dict[bytes, int], train: bool
@@ -88,16 +153,7 @@ class BPETokenizer:
                 groups.extend(indices_array)
         return groups
 
-    def encode_iterable(self, fp) -> Generator[int]:
-        text = fp.read()
-        if isinstance(text, bytes):
-            text = text.decode("utf-8")
-
-        indices = self.encode(text)
-        for idx in indices:
-            yield idx
-
-    def encode(self, text: str) -> List[int]:
+    def _internal_encode(self, text: str) -> List[int]:
         if self._special_token_pattern is not None:
             text_array = re.split(self._special_token_pattern, text)
         else:
@@ -155,36 +211,68 @@ class BPETokenizer:
                 node = node.r
         return indices
 
-    def decode(self, indices: List[int]):
+    def _internal_decode(self, indices: List[int]):
         return b"".join(self.vocab[idx] for idx in indices).decode(
             "utf-8", errors="replace"
         )
 
     @staticmethod
-    def from_params(
-        vocab: Dict[int, bytes],
-        merges: List[Tuple[bytes, bytes]],
-        special_tokens: Optional[List[str]] = None,
-    ) -> "BPETokenizer":
-        tokenizer = BPETokenizer(special_tokens)
-        tokenizer.vocab = vocab
-        tokenizer._vocab_dic = {v: k for k, v in tokenizer.vocab.items()}
-        tokenizer.merges = {}
-        for b1, b2 in merges:
-            merge = Merge(tokenizer._vocab_dic[b1], tokenizer._vocab_dic[b2])
-            idx = tokenizer._vocab_dic[b1 + b2]
-            tokenizer.merges[idx] = merge
-        return tokenizer
+    def from_file(file_path) -> "BPETokenizer":
+        s2i = {v: k for k, v in gpt2_bytes_to_unicode().items()}
 
-    def export_params(self):
-        vocab = self.vocab
+        with open(file_path, "r", encoding="utf-8") as f:
+            obj = json.loads(f.read())
+        special_tokens = obj["special_tokens"]
+        vocab = obj["vocab"]
+        merges = obj["merges"]
+        tokenizer_vocab = {k: bytes([s2i[i] for i in v]) for k, v in enumerate(vocab)}
+        tokenizer_vocab_dic = {v: k for k, v in tokenizer_vocab.items()}
+        tokenizer_merges = {}
+        for s in merges:
+            s1, s2 = s.split(" ")
+            b1 = bytes([s2i[i] for i in s1])
+            b2 = bytes([s2i[i] for i in s2])
+            merge = Merge(tokenizer_vocab_dic[b1], tokenizer_vocab_dic[b2])
+            idx = tokenizer_vocab_dic[b1 + b2]
+            tokenizer_merges[idx] = merge
+        return BPETokenizer(tokenizer_vocab, tokenizer_merges, special_tokens)
+
+    def save(self, path):
+        i2s = gpt2_bytes_to_unicode()
+
+        vocab = [None] * (max(self.vocab.keys()) + 1)
+        for k, v in self.vocab.items():
+            vocab[k] = "".join([i2s[i] for i in v])
         merges = [
-            (self.vocab[self.merges[k].idx0], self.vocab[self.merges[k].idx1])
+            "".join([i2s[i] for i in self.vocab[self.merges[k].idx0]])
+            + " "
+            + "".join([i2s[i] for i in self.vocab[self.merges[k].idx1]])
             for k in sorted(self.merges.keys())
         ]
-        return vocab, merges
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(
+                {
+                    "special_tokens": self.special_tokens,
+                    "vocab": vocab,
+                    "merges": merges,
+                },
+                f,
+                ensure_ascii=False,
+            )
 
-    def train(self, text: str, vocab_size: int):
+    @staticmethod
+    def build_special_token_pattern(special_tokens):
+        return f"({'|'.join(re.escape(s) for s in sorted(special_tokens, key=lambda s: -len(s)))})"
+
+    def train(
+        self, text: str, vocab_size: int, special_tokens: Optional[List[str]] = None
+    ):
+        if special_tokens is not None:
+            self.special_tokens = special_tokens
+            self._special_token_pattern = self.build_special_token_pattern(
+                self.special_tokens
+            )
+
         if self._special_token_pattern is not None:
             text_array = re.split(self._special_token_pattern, text)
             initial_vocab = [s.encode("utf-8") for s in self.special_tokens]
