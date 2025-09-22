b22bb58c78176b19f92877b36a1542d7758293e6
tigertang <tigertang.zju@qq.com>
2025-09-22 23:48:20 +0800

    Add matrix mul tall and skinny

--- mytorch/native/cuda/conv.cu/source.cu
+++ mytorch/native/cuda/conv.cu/source.cu
@@ -8,18 +8,18 @@
 
 class ColMajorLayout {
  public:
-  int height, width;
-  __device__ __host__ ColMajorLayout(int height, int width)
-      : height(height), width(width) {}
-  __device__ __host__ int Index(int x, int y) { return x + y * height; }
+  int matrix_height, matrix_width;
+  __device__ __host__ ColMajorLayout(int matrix_height, int matrix_width)
+      : matrix_height(matrix_height), matrix_width(matrix_width) {}
+  __device__ __host__ int Index(int x, int y) { return x + y * matrix_height; }
 };
 
 class RowMajorLayout {
  public:
-  int height, width;
-  __device__ __host__ RowMajorLayout(int height, int width)
-      : height(height), width(width) {}
-  __device__ __host__ int Index(int x, int y) { return x * width + y; }
+  int matrix_height, matrix_width;
+  __device__ __host__ RowMajorLayout(int matrix_height, int matrix_width)
+      : matrix_height(matrix_height), matrix_width(matrix_width) {}
+  __device__ __host__ int Index(int x, int y) { return x * matrix_width + y; }
 };
 
 template <typename T, typename L>
@@ -216,6 +216,52 @@ __global__ void MatrixMulTallAndSkinny(uint32_t m, uint32_t n, uint32_t k,
   }
 }
 
+template <typename T, uint32_t block_size, uint32_t block_size_k, typename M1,
+          typename M2, typename M3>
+__global__ void MatrixMulTallAndSkinny(uint32_t m, uint32_t n, uint32_t k,
+                                       M1 d_a, M2 d_b, M3 d_c, T *bias) {
+  extern __shared__ char shared[];
+  T *s_a_mem = (T *)shared;
+  T *s_b_mem = (T *)shared + block_size * block_size_k;
+  auto s_a = MatrixWrapper<T, ColMajorLayout>{
+      s_a_mem, ColMajorLayout(block_size, block_size_k)};
+  auto s_b = MatrixWrapper<T, RowMajorLayout>{
+      s_b_mem, RowMajorLayout(block_size_k, block_size)};
+
+  for (int i = blockIdx.x; i < (m + block_size - 1) / block_size;
+       i += gridDim.x) {
+    for (int j = blockIdx.y; j < (n + block_size - 1) / block_size;
+         j += gridDim.y) {
+      T regs[block_size * block_size] = {(T)0};
+
+      for (int idx = threadIdx.x; idx < k; idx += block_size_k) {
+        __syncthreads();
+        for (int p = 0; p < block_size; p++) {
+          s_a.Set(p, threadIdx.x, d_a.Get(i * block_size + p, idx));
+          s_b.Set(threadIdx.x, p, d_b.Get(idx, j * block_size + p));
+        }
+        __syncthreads();
+
+        for (int p = 0; p < block_size * block_size; p++) {
+          regs[p] += s_a.Get(p % block_size, threadIdx.x) *
+                     s_b.Get(threadIdx.x, p / block_size);
+        }
+      }
+
+      if (threadIdx.x == 0) {
+        for (int p = 0; p < block_size * block_size; p++)
+          d_c.Set(i * block_size + p % block_size,
+                  j * block_size + p / block_size,
+                  bias != nullptr ? bias[j * block_size + p / block_size] : 0);
+      }
+      __syncthreads();
+      for (int p = 0; p < block_size * block_size; p++)
+        d_c.Add(i * block_size + p % block_size,
+                j * block_size + p / block_size, regs[p]);
+    }
+  }
+}
+
 template <typename T, bool reduce, typename M1, typename M2, typename M3>
 void MatrixMul(M1 a, M2 b, M3 c, T *bias, cudaStream_t stream) {
   uint32_t m = c.layout.matrix_height;
@@ -223,10 +269,23 @@ void MatrixMul(M1 a, M2 b, M3 c, T *bias, cudaStream_t stream) {
   uint32_t k = a.layout.matrix_width;
 
   if (m * n <= 1024) {
-    dim3 block = {32, 1, 1};
-    dim3 grid = {256, 1, 1};
-    int shared_bytes = m * n * sizeof(T);
-    MatrixMulTallAndSkinny<T, 1024>
+    static constexpr int block_size = 2;
+    static constexpr int block_size_k = 512;
+    dim3 block = {block_size_k, 1, 1};
+    dim3 grid = {16, 16, 1};
+    int shared_bytes = 2 * block_size * block_size_k * sizeof(T);
+    MatrixMulTallAndSkinny<T, block_size, block_size_k>
+        <<<grid, block, shared_bytes, stream>>>(m, n, k, a, b, c, bias);
+  } else if (m * n <= 65536) {
+    static constexpr uint32_t block_size_m = 32, block_size_n = 32,
+                              block_size_k = 32;
+    constexpr int num_warps = 8;
+    dim3 block = {32 * num_warps, 1, 1};
+    dim3 grid = {(m + block_size_m - 1) / block_size_m,
+                 (n + block_size_n - 1) / block_size_n, 1};
+    int shared_bytes = (block_size_m + block_size_n) * block_size_k * sizeof(T);
+
+    MatrixMulGeneral<T, block_size_m, block_size_n, block_size_k, reduce>
         <<<grid, block, shared_bytes, stream>>>(m, n, k, a, b, c, bias);
   } else {
     static constexpr uint32_t block_size_m = 64, block_size_n = 64,
